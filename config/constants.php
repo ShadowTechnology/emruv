0a3613410b491c4dd6bd878e050e27814d034cef
ShadowTechnology <aravindaravind400@gmail.com>
2024-10-28 23:49:57 +0530

    7

---
+++ config/constants.php
@@ -0,0 +1,18 @@
+<?php
+
+return [  
+
+    'site_name' => 'Emruv',
+
+	'mrp_text' => 'Rs.',
+	'money_symbol' => '₹',
+	'mrp_symbol' => '₹',
+
+    'razor_api_key' => 'rzp_test_qTNFnI9BxKgNDh',
+    'razor_api_secret' => '7WbfGq4xqAYnx2Gt8KLrVHNU', 
+    'razor_currency' => 'INR',
+
+    'support_mail' => 'supportemruv@gmail.com',
+
+    'APP_IMAGE_URL' => 'http://3.12.170.234/emruv/public/',
+];
